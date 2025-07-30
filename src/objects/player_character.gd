ee451d08da84f9a82fb9598d3e919500e1a15305
Gabor Heja <gheja@ubuntu1.w830g8.git.stdio.hu>
2025-07-31 00:16:42 +0200

    Reworked mouse look and player movement - thx https://docs.godotengine.org/en/4.3/tutorials/3d/spring_arm.html

--- src/objects/player_character.gd
+++ src/objects/player_character.gd
@@ -3,14 +3,19 @@ extends CharacterBody3D
 @export var max_speed = 5
 @export var is_actively_controlled = true
 
+# TODO: should be global
+@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
+
 var look_at_position = Vector3.FORWARD * 1000
 
-func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+@onready var camera_pivot = $Node3D/CameraPivot
+@onready var camera = $Node3D/CameraPivot/SpringArm3D/Camera3D
+@onready var lower_body = $Visuals/LowerBody
+@onready var upper_body = $Visuals/UpperBody
+
 
 func _process(delta: float) -> void:
 	update_wheels_direction()
-	update_camera_position()
 
 func update_wheels_direction():
 	var velocity_xz = Vector3(self.velocity.x, 0.0, self.velocity.z)
@@ -18,15 +23,21 @@ func update_wheels_direction():
 	if velocity_xz.length() > 0.1:
 		look_at_position = self.global_position + velocity_xz
 	
-	$Visuals/LowerBody.look_at(look_at_position)
-
-func update_camera_position():
-	$CameraContainer.rotation.y -= Input.get_last_mouse_velocity().x * 0.0001
+	upper_body.rotation.y = camera_pivot.rotation.y
+	lower_body.look_at(look_at_position)
 
 func _physics_process(delta: float) -> void:
 	var vec = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	vec = vec.rotated(-camera_pivot.rotation.y)
+	
 	var vec3 = Vector3(vec.x, 0.0, vec.y)
 	
 	self.velocity = vec3 * max_speed + get_gravity()
 	
 	self.move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		camera_pivot.rotation.x -= event.relative.y * mouse_sensitivity
+		camera_pivot.rotation.x = clampf(camera_pivot.rotation.x, deg_to_rad(-75), deg_to_rad(-15))
+		camera_pivot.rotation.y += -event.relative.x * mouse_sensitivity
--- src/scenes/main_scene.gd
+++ src/scenes/main_scene.gd
@@ -1,12 +1,4 @@
 extends Node3D
 
-var camera_position_obj: Node3D
-var camera_target_obj: Node3D
-
 func _ready() -> void:
-	camera_position_obj = $LevelBase/LevelObjectContainer/PlayerCharacter2/CameraContainer/CameraPosition
-	camera_target_obj = $LevelBase/LevelObjectContainer/PlayerCharacter2/CameraContainer/CameraTarget
-
-func _process(delta: float) -> void:
-	$Camera3D.global_position = camera_position_obj.global_position
-	$Camera3D.look_at(camera_target_obj.global_position)
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
