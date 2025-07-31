f6d58446d721705cc32a7e1400f6241b038dfc3d
Gabor Heja <gheja@ubuntu1.w830g8.git.stdio.hu>
2025-07-31 02:09:28 +0200

    Stop button now handles areas too

--- src/objects/object_stop_button.gd
+++ src/objects/object_stop_button.gd
@@ -2,3 +2,6 @@ extends Node3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	Signals.stop_pressed.emit()
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	Signals.stop_pressed.emit()
