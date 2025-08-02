7db96f46a3592cc9c8b127f86a28e8558f5a0c11
Gabor Heja <gheja@ubuntu1.w830g8.git.stdio.hu>
2025-08-02 11:41:39 +0200

    Reworked door activation change handling

--- src/objects/object_door.gd
+++ src/objects/object_door.gd
@@ -5,17 +5,23 @@ var activation_state = false
 
 func _ready() -> void:
 	Signals.trigger_activation_changed.connect(_on_trigger_activation_changed)
+	$AnimationPlayer.play("main_action")
+	$AnimationPlayer.pause()
 
 func handle_new_state():
-	$AnimationPlayer.stop()
+	var pos = $AnimationPlayer.current_animation_position
 	
 	if activation_state == true:
-		$AnimationPlayer.play("door_open")
+		$AnimationPlayer.play()
 	else:
-		$AnimationPlayer.play("door_close")
+		$AnimationPlayer.play_backwards()
+	
+	$AnimationPlayer.seek(pos)
 
 func _on_trigger_activation_changed(group_id: int, state: bool):
 	if group_id == self.activation_group_id:
+		# there might be two triggers
+		# BUG? when one of the trigger stops it deactivates this
 		if state == activation_state:
 			return
 			
